9d5f19290cbf110ccd1be163a3b402c2544725b7
Ahmet Can Yürekli <ayurekl97@gmail.com>
2022-08-20 13:38:20 +0300

    axios added

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,3 @@ hamburger.onclick = function () {
   navBar = document.querySelector(".nav-bar");
   navBar.classList.toggle("active");
 };
-
-var PModal1 = document.getElementById("#PModal1");
-console.log(PModal1);
