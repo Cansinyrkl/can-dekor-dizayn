b91b82843960cfd9a5a518e80afc211f067daded
Ahmet Can Yürekli <ayurekl97@gmail.com>
2022-08-20 20:26:31 +0300

    modals done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,30 @@
-hamburger = document.querySelector(".hamburger");
+const hamburger = document.querySelector(".hamburger");
+const navBar = document.querySelector(".nav-bar");
+const navBarContainer = document.querySelector("#navBarContainer");
+
 hamburger.onclick = function () {
-  navBar = document.querySelector(".nav-bar");
   navBar.classList.toggle("active");
 };
+
+function addListenerMulti(element, eventNames, listener) {
+  var events = eventNames.split(' ');
+  for (var i = 0, iLen = events.length; i < iLen; i++) {
+    element.addEventListener(events[i], listener, false);
+  }
+}
+
+addListenerMulti(window, 'scroll resize', function () {
+  let newWidth = window.innerWidth;
+  let scroll = window.scrollY;
+  if (scroll > 100) {
+    console.log(scroll);
+    navBarContainer.style.height = "150px";
+    navBarContainer.style.transition = "2s";
+
+  } else {
+    navBarContainer.style.height = "100px";
+    navBarContainer.style.transition = "2s";
+  }
+
+});
+
