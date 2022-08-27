5ca183a3dada1e9b9f7a078abd0b5c07d1784907
Ahmet Can Yürekli <ayurekl97@gmail.com>
2022-08-27 19:00:03 +0300

    product added card, hide show

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,9 @@
 const hamburger = document.querySelector(".hamburger");
 const navBarContainer = document.querySelector("#navBarContainer");
 const above = document.querySelector("#above");
-const productDiv = document.getElementById("productDiv")
+const productDiv = document.getElementById("productDiv");
+const btnShow = document.getElementById("btnShow");
+const btnHide = document.getElementById("btnHide");
 
 hamburger.onclick = function () {
   navBar = document.querySelector(".nav-bar");
@@ -9,45 +11,39 @@ hamburger.onclick = function () {
 };
 
 function addListenerMulti(element, eventNames, listener) {
-  var events = eventNames.split(' ');
+  var events = eventNames.split(" ");
   for (var i = 0, iLen = events.length; i < iLen; i++) {
     element.addEventListener(events[i], listener, false);
   }
 }
 
-addListenerMulti(window, 'scroll resize', function () {
+addListenerMulti(window, "scroll resize", function () {
   let newWidth = window.innerWidth;
   let scroll = window.scrollY;
-  if (scroll > 220) {
-    console.log(scroll);
+  if (scroll > 225) {
     navBarContainer.style.height = "100px";
     navBarContainer.style.transition = "0.4s";
     navBarContainer.style.borderBottomLeftRadius = "19%";
     navBarContainer.style.borderBottomRightRadius = "19%";
     above.style.visibility = "visible";
-
   } else {
     navBarContainer.style.height = "90px";
     navBarContainer.style.transition = "0.6s";
     navBarContainer.style.borderBottomLeftRadius = "0%";
     navBarContainer.style.borderBottomRightRadius = "0%";
     above.style.visibility = "hidden";
-
   }
-
 });
-
 
 const onload = () => {
   const div = document.getElementById("modalDiv");
-  for (let i = 1; i <= 27; i++) {
+  for (let i = 1; i <= 26; i++) {
     axios.get(`productModals/PModal${i}.html`).then((res) => {
       const data = res.data;
       div.innerHTML += data;
-    })
+    });
   }
-}
-
+};
 
 const products = [
   {
@@ -284,30 +280,103 @@ const products = [
     imgAlt: "Resim",
     imgStyle: "",
   },
+];
 
-]
+const kesilen1 = products.slice(0, 4);
+const kesilen2 = products.slice(5, 15);
+const kesilen3 = products.slice(15);
+let count = 0;
 
-
-products.map((product) => {
-  productDiv.innerHTML +=
-    `
-    <div class="a-box"  data-bs-target="${product.databsTarget}" >
+kesilen1.map((kesilen1) => {
+  productDiv.innerHTML += `
+    <div class="a-box" data-bs-toggle="modal"  data-bs-target="${kesilen1.databsTarget}">
     <div class="img-container">
         <div class="img-inner">
             <div class="inner-skew">
                 <img
-                    src="${product.imgSrc}"> alt="${product.imgAlt}"
+                    src="${kesilen1.imgSrc}"> alt="${kesilen1.imgAlt}"
             </div>
         </div>
     </div>
     <div class="text-container">
-        <h3>${product.name}</h3>
+        <h3>${kesilen1.name}</h3>
         <div>
-            ${product.description}
+            ${kesilen1.description}
         </div>
     </div>
-    `
-})
+    `;
+});
 
+const btnGoster = () => {
+  count++;
+  if (count === 1) {
+    kesilen2.map((kesilen2) => {
+      productDiv.innerHTML += `
+      <div class="a-box" data-bs-toggle="modal"  data-bs-target="${kesilen2.databsTarget}">
+      <div class="img-container">
+          <div class="img-inner">
+              <div class="inner-skew">
+                  <img
+                      src="${kesilen2.imgSrc}"> alt="${kesilen2.imgAlt}"
+              </div>
+          </div>
+      </div>
+      <div class="text-container">
+          <h3>${kesilen2.name}</h3>
+          <div>
+              ${kesilen2.description}
+          </div>
+      </div>
+      `;
+    });
+  } else if (count === 2) {
+    kesilen3.map((kesilen3) => {
+      productDiv.innerHTML += `
+      <div class="a-box" data-bs-toggle="modal"  data-bs-target="${kesilen3.databsTarget}">
+      <div class="img-container">
+          <div class="img-inner">
+              <div class="inner-skew">
+                  <img
+                      src="${kesilen3.imgSrc}"> alt="${kesilen3.imgAlt}"
+              </div>
+          </div>
+      </div>
+      <div class="text-container">
+          <h3>${kesilen3.name}</h3>
+          <div>
+              ${kesilen3.description}
+          </div>
+      </div>
+      `;
+    });
+    count = 0;
+    btnShow.style.display = "none";
+    btnHide.style.display = "block";
+  }
+};
 
+const btnGizle = () => {
+  productDiv.innerHTML = "";
 
+  kesilen1.map((kesilen1) => {
+    productDiv.innerHTML += `
+    <div class="a-box" data-bs-toggle="modal"  data-bs-target="${kesilen1.databsTarget}">
+    <div class="img-container">
+        <div class="img-inner">
+            <div class="inner-skew">
+                <img
+                    src="${kesilen1.imgSrc}"> alt="${kesilen1.imgAlt}"
+            </div>
+        </div>
+    </div>
+    <div class="text-container">
+        <h3>${kesilen1.name}</h3>
+        <div>
+            ${kesilen1.description}
+        </div>
+    </div>
+    `;
+  });
+  btnShow.style.display = "block";
+  btnHide.style.display = "none";
+};
